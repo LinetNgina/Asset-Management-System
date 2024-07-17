518e3940454f5ceab7f166735da4569087a3ca53
LinetNgina <linetngina097@gmail.com>
2024-07-17 15:45:13 +0300

    First Repo

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class MaintenanceRecord(models.Model):
+    category = models.CharField(max_length=100)
+    asset_id = models.CharField(max_length=50)
+    comments = models.TextField()
+
+    def __str__(self):
+        return f"{self.category} - {self.asset_id}"
+
+class MaintenanceRequest(models.Model):
+    subject = models.CharField(max_length=100)
+    description = models.TextField()
+    submitted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
+
+class YourModel(models.Model):
+    field1 = models.CharField(max_length=100)
+    field2 = models.TextField()
+
+    def __str__(self):
+        return self.field1
